0278250c3011447797085fdd19397ee794347abf
sivav15 <sivanathanv1999@gmail.com>
2023-03-07 13:19:00 +0530

    ss

--- src/components/UnassignedStudent.js
+++ src/components/UnassignedStudent.js
@@ -1,58 +1,66 @@
 import axios from 'axios';
-import React,{ useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table';
 import { env } from '../config';
+import Loading from './loading/Loading'
+
 
 function UnassignedStudent() {
-  const [studentData,setStudentData] = useState([]);
-  
+  const [studentData, setStudentData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
 
   useEffect(() => {
     getStudent();
-}, []);
+  }, []);
 
 
   const getStudent = async () => {
-    try { 
-      let value = await axios.get(`${env.api}/unAssigned-student`);  
+    try {
+      setLoading(true);
+      let value = await axios.get(`${env.api}/unAssigned-student`);
       setStudentData(value.data.data);
+      setLoading(false);
     } catch (error) {
-        console.log(error);
+      setLoading(false);
+      console.log(error);
     }
   };
 
   return (
     <div>
 
-    <div className='table'>
-    <h3 className='text-center'> Unassigned Student</h3>
-    <Table striped bordered hover>
-    <thead>
-      <tr>
-      <th>#</th>
-          <th> Name</th>
-          <th>Email</th>
-          <th>Mobile</th>
-      </tr>
-    </thead>
-    <tbody>
-    {
-        studentData.length > 0  && studentData.map((item,index)=>{
-          return  <tr>
-          <td>{index + 1}</td>
-          <td>{item.firstName} {item.lastName}</td>
-          <td>{item.email}</td>
-          <td>{item.mobile}</td>
-        </tr>
-         })
-     }
+      <div className='table'>
+        <h3 className='text-center'> Unassigned Student</h3>
+        {
+          loading ? <div className='d-flex justify-content-center align-items-center'> <Loading width={"3.4rem"} /> </div> :
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th> Name</th>
+                  <th>Email</th>
+                  <th>Mobile</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  studentData.length > 0 && studentData.map((item, index) => {
+                    return <tr>
+                      <td>{index + 1}</td>
+                      <td>{item.firstName} {item.lastName}</td>
+                      <td>{item.email}</td>
+                      <td>{item.mobile}</td>
+                    </tr>
+                  })
+                }
 
-    </tbody>
-  </Table>
+              </tbody>
+            </Table>
+        }
+      </div>
+
     </div>
-
-  </div>
   )
 }
 
